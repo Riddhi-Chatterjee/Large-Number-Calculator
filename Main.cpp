92660d86929f901a919413323dfe5ff365a004ef
Riddhi-Chatterjee <riddhich@rediffmail.com>
2023-11-17 20:08:37 +0530

    setting internal base power

--- Main.cpp
+++ Main.cpp
@@ -23,9 +23,10 @@ void strToNum(struct Number &num, string s, baseConverter *bConv);
 int main()
 {
     int INTERNAL_BASE_POWER; //Base = 2^(INTERNAL_BASE_POWER) for internal computations
-    cout<<"For internal computations, the base is 2^m. Enter the value of m:"<<endl;
-    cin>>INTERNAL_BASE_POWER;
-    cout<<endl;
+    //cout<<"For internal computations, the base is 2^m. Enter the value of m:"<<endl;
+    //cin>>INTERNAL_BASE_POWER;
+    INTERNAL_BASE_POWER = 10; //Setting INTERNAL_BASE_POWER to 10 for optimal performance
+    //cout<<endl;
     int PRECISION_PAD_DIGITS = 6; //Padding extra 6 digits of precision as compared to the required precision.
     int iBase;
     iBase = (int)pow(2, INTERNAL_BASE_POWER); //The base for internal computations
